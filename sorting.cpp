29f4835d7b5366fead97a523cf15e69fb264a5db
Rohan <rohankumar5803@gmail.com>
2023-02-05 10:16:18 +0530

    First commit

---
+++ sorting.cpp
@@ -0,0 +1,20 @@
+//C++ program to demonstrate sort function
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+    sort(a,a+3);
+    for(int i=0; i<n; i++)
+        cout<<a[i]<<" ";
+
+    cout<<endl;
+
+    sort(a,a+n, greater<int>());
+    for(int i=0; i<n; i++)
+        cout<<a[i]<<" ";
+}
